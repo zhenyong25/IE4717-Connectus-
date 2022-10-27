6d9355882ecf460e4d2515c735f17aede5923905
zhenyong25 <zhenyong25@gmail.com>
2022-10-27 23:22:16 +0800

    Created database for user signup

--- sql/user.sql
+++ sql/user.sql
@@ -1,7 +1,8 @@
 create table user {
     id int unsigned not null auto_increment primary key,
-    email-address,
-    password,
-    name,
-    contact
-}
+    email_address char(50) not null,
+    password char(50) not null,
+    retype_password char(50) not null,
+    name char(50) not null,
+    contact char(50) not null 
+}; 
